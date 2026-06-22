abb2d89b9196d211a88d8a3a8d0ffdfa2a2a7760
agroventures <sandaru@agroventures.lk>
2026-06-22 14:01:24 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,20 @@ import Products from './pages/Products';
 import Contact from './pages/Contact';
 import About from './pages/About';
 import ProcessingFacility from './pages/ProcessingFacility';
-import Media from './pages/Media';
+// import Media from './pages/Media';
 import ScrollToTop from './components/shared/ScrollToTop';
-import Producers from './pages/Producers';
+// import Producers from './pages/Producers';
 import TermsAndConditions from './pages/TermsAndConditions';
 import PrivacyPolicy from './pages/PrivacyPolicy';
 import QualityCertifications from './pages/QualityCertifications';
-import EcoPlus from './pages/EcoPlus';
+// import EcoPlus from './pages/EcoPlus';
 import Loader from './components/shared/Loader';
 import { useEffect, useState } from 'react';
 import Awards from './pages/Awards';
-import GlobalPresence from './pages/GlobalPresence';
-import Sustainability from './pages/Sustainability';
-import PartnerWithUs from './pages/PartnerWithUs';
-import EcoSystem from './pages/EcoSystem';
+// import GlobalPresence from './pages/GlobalPresence';
+// import Sustainability from './pages/Sustainability';
+// import PartnerWithUs from './pages/PartnerWithUs';
+// import EcoSystem from './pages/EcoSystem';
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -43,15 +43,15 @@ function App() {
         <Route path="/contact" element={<Contact />} />
         <Route path="/about" element={<About />} />
         <Route path="/awards" element={<Awards />} />
-        <Route path="/global-presence" element={<GlobalPresence />} />
-        <Route path="/sustainability" element={<Sustainability />} />
-        <Route path="/partner-with-us" element={<PartnerWithUs />} />
-        <Route path="/eco-system" element={<EcoSystem />} />
-        <Route path="/producers" element={<Producers />} />
+        {/* <Route path="/global-presence" element={<GlobalPresence />} /> */}
+        {/* <Route path="/sustainability" element={<Sustainability />} /> */}
+        {/* <Route path="/partner-with-us" element={<PartnerWithUs />} /> */}
+        {/* <Route path="/eco-system" element={<EcoSystem />} /> */}
+        {/* <Route path="/producers" element={<Producers />} /> */}
         <Route path="/processing-facilities" element={<ProcessingFacility />} />
-        <Route path="/media" element={<Media />} />
+        {/* <Route path="/media" element={<Media />} /> */}
         <Route path="/quality-certifications" element={<QualityCertifications />} />
-        <Route path="/eco-plus" element={<EcoPlus />} />
+        {/* <Route path="/eco-plus" element={<EcoPlus />} /> */}
         <Route path="/terms" element={<TermsAndConditions />} />
         <Route path="/privacy" element={<PrivacyPolicy />} />
       </Routes>
