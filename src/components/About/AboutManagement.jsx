f48d7fb090875de67ef5a6ea1f7b77260864b150
Dilnuwan Weerasuriya <dilnuwanweerasuriya@gmail.com>
2026-06-23 11:09:35 +0530

    Update AboutManagement.jsx

--- src/components/About/AboutManagement.jsx
+++ src/components/About/AboutManagement.jsx
@@ -19,7 +19,7 @@ const founderHighlights = [
 const founder = {
   id: "founder",
   name: "Dr. Sarath Ranaweera",
-  role: "Founder & Consultant",
+  role: "Consultant",
   img: "https://pub-8476bede5a4146e8b7731cfe515f1c3b.r2.dev/biofoodslk/About/Dr.Sarath-Ranaweera-2.webp",
 };
 
